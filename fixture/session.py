8a512e36e30e890b8f11e943342e0c1ea81dcd1d
slobodchikova <Irina.Slobodchikova@interfax.ru>
2018-09-03 11:50:39 +0300

    conftest+application

--- fixture/session.py
+++ fixture/session.py
@@ -57,48 +57,46 @@ class SessionHelperSM:
     def __init__(self, app):
         self.app = app
 
-    def Marker_login(self, username, password):
+    def sm_login(self, username, password):
         wd = self.app.wd
         self.app.open_sm_home_page()
-        wd.find_element_by_css_selector("button.mainbtn").click()
-        wd.find_element_by_name("username").click()
-        wd.find_element_by_name("username").clear()
-        wd.find_element_by_name("username").send_keys(username)
-        wd.find_element_by_name("password").click()
-        wd.find_element_by_name("password").clear()
-        wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_css_selector("button.navbar-btn.mainbtn").click()
+        wd.find_element_by_css_selector("a.button.login").click()
+        wd.find_element_by_name("UserName").click()
+        wd.find_element_by_name("UserName").clear()
+        wd.find_element_by_name("UserName").send_keys(username)
+        wd.find_element_by_name("Password").click()
+        wd.find_element_by_name("Password").clear()
+        wd.find_element_by_name("Password").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='form-login']//button[.='Войти']").click()
 
-    def Marker_logout(self):
+    def sm_logout(self):
         wd = self.app.wd
-        wd.find_element_by_css_selector("div.headernav__uname").click()
-        wd.find_element_by_link_text("Выйти из системы").click()
+        wd.find_element_by_css_selector("a.icon-logout").click()
 
-    def ensure_logout(self):
+
+    def ensure_logout_sm(self):
         wd = self.app.wd
-        if self.is_logged_in():
-            self.Marker_logout()
+        if self.is_logged_in_sm():
+            self.sm_logout()
 
-    def is_logged_in(self):
+    def is_logged_in_sm(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_css_selector("img.headernav__icon.notifyico")) > 0
+        return len(wd.find_elements_by_css_selector("span.hdr_user-menu_name")) > 0
 
-    def is_logged_in_as(self, username):
+    def is_logged_in_as_sm(self, username):
         wd = self.app.wd
-        return self.get_logged_user() == username
+        return self.get_logged_user_sm() == username
 
-    def get_logged_user(self):
+    def get_logged_user_sm(self):
         wd = self.app.wd
-        wd.find_element_by_css_selector("div.headernav__uname").click()
-        wd.find_element_by_link_text("Личный кабинет").click()
+        text = wd.find_element_by_xpath("//div[30]/header/div[2]/div/a[1]/span").text
+        return text
 
-        return wd.find_element_by_xpath("//ng-component/main/ng-component/div/div[2]/div/div[1]/div[1]/div/div[2]/span").text[10:-0]
-
-    def ensure_login(self, username, password):
+    def ensure_login_sm(self, username, password):
         wd = self.app.wd
-        if self.is_logged_in():
-            if self.is_logged_in_as(username):
+        if self.is_logged_in_sm():
+            if self.is_logged_in_as_sm(username):
                 return
             else:
-                self.Marker_logout()
-        self.Marker_login(username, password)
+                self.sm_logout()
+        self.sm_login(username, password)
--- test/test_login.py
+++ test/test_login.py
@@ -1,9 +1,15 @@
 
 
-def test_login(app):
+#def test_marker_login(app):
+#    username = app.username
+#    password = app.password
+#    app.session.ensure_login_marker(username, password)
+#    user = app.session.get_logged_user_marker()
+#    assert username == user
+
+def test_marker_login(app):
     username = app.username
     password = app.password
-    app.session.ensure_login_marker(username, password)
-    user = app.session.get_logged_user_marker()
+    app.sessionSM.sm_login(username, password)
+    user = app.sessionSM.get_logged_user_sm()
     assert username == user
-
